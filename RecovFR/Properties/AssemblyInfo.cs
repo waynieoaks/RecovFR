de664d7087c6ba60858c97d36cd980085ef9703a
Waynieoaks <5962977+waynieoaks@users.noreply.github.com>
2020-05-10 16:53:16 +0100

    Initial commit to Git

---
+++ RecovFR/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Rage.Attributes;
+
+[assembly: Plugin("RecovFR", Description = "Backup and restore some game play on ther fly...", PrefersSingleInstance = true, Author = "Waynieoaks")]
+[assembly: AssemblyTitle("RecovFR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RecovFR")]
+[assembly: AssemblyCopyright("Copyright ©  2020 - Waynieoaks")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08c55d48-5132-406c-ae77-bfdb3b7c8422")]
+
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
